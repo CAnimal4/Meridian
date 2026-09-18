982d8232fe167387748e54f54f6d0970f9696407
CAnimal4 <clarkbythebay@gmail.com>
2026-09-18 16:45:46 -0700

    Harden modal closing and password feedback

--- app.js
+++ app.js
@@ -4626,6 +4626,7 @@ mean/nice
 
     openModal(overlay, focusEl) {
       this.lastFocus = document.activeElement;
+      overlay.hidden = false;
       overlay.style.display = 'flex';
       // Basic focus management
       setTimeout(() => {
@@ -4634,6 +4635,7 @@ mean/nice
     },
 
     closeModal(overlay) {
+      overlay.hidden = true;
       overlay.style.display = 'none';
       // Return focus
       if (this.lastFocus && this.lastFocus.focus) {
